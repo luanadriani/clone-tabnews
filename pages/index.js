98443c00506a994f4c59761a9f01c07f20067fbd
Luan Adriani <luanadriani2011@gmail.com>
2023-07-05 20:02:48 +0000

    altera a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Pamela, eu te amo! Se você me ama, dá uma risadinha!</h1>
+    return <h1>Pamela, eu te amo! Se você me ama, dá uma risadinha! 😍😍😍</h1>
 }
 
 export default Home;
